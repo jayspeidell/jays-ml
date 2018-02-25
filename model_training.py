1de00512f820457f8f50f5d9ebcc85f1c26028c8
Unknown <jayspeidell@gmail.com>
2018-02-24 19:27:23 -0800

    MulitLabel Class Functional
    The class that creates a multi-label model from regular classification models is fully functional now.

--- model_training.py
+++ model_training.py
@@ -1,8 +1,16 @@
 import numpy as np
 from sklearn.base import clone
 from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import fbeta_score
 
 class MultiLabel(): 
+    """
+    A multi-label model class that builds on any machine learning model class 
+    with standard fit and predict methods. Arguments are the model you want to
+    use and a list of the class labels for your data. Stores a list of models, 
+    one for each classification label. 
+	"""
     def __init__(self, base_model, labels):
         self.base_model = base_model
         self.labels = labels
@@ -11,24 +19,31 @@ class MultiLabel():
         self.generate_copies()
         self.scores = []
 
-        self.initialize()
+        self.initialize()    
         
-    def generate_copies(self):
-        for _ in range(self.num_labels):
-            self.models.append(clone(self.base_model))
     
     def initialize(self):
         print('Multi-Label Object Initialized')
         print(('%d Labels: "' + '", "'.join(self.labels) +'"') % self.num_labels)
         print(self.base_model)
+        
+        
+    def generate_copies(self):
+        """Internal, creates a cloned model for each label."""
+        for _ in range(self.num_labels):
+            self.models.append(clone(self.base_model))
+    
     
     def cm_heatmap(self, arr, title):
+        """Internal. Only called by scoring function."""
         plt.figure('cm_heatmap')
         plt.title(title + ' confusion matrix')
         sns.heatmap(arr, square=True, annot=True, cmap='YlOrRd', fmt='g')
         plt.show()
+        
 
     def scoring(self, y_test, y_pred, i):
+        """Internal. Called during model testing."""
         cm = confusion_matrix(y_test, y_pred)
         precision = cm[1][1] / (cm[1][1] + cm[0][1])
         print('Precision: %.3f' % precision)
@@ -40,7 +55,9 @@ class MultiLabel():
         self.scores.append([self.labels[i], precision, recall, f1_score])
         print()  
     
-    def fit(self, X, y, X_test, y_test):
+    
+    def fit(self, X, y):
+        """Simply fit the model to your data. Does not test or populate scores."""
         for i, model in enumerate(self.models):
             print('Fitting model %d for label %s.' % (i+1, self.labels[i]))
             model.fit(X, y[:,i])
@@ -48,7 +65,9 @@ class MultiLabel():
             self.models[i] = model
         print('Training complete.')
     
+    
     def fit_test(self, X, y, X_test, y_test):
+        """Fit your model to the data and test it, pupulating scores."""
         for i, model in enumerate(self.models):
             print('Fitting model %d for label %s.' % (i+1, self.labels[i]))
             model.fit(X, y[:,i])
@@ -57,14 +76,17 @@ class MultiLabel():
             self.scoring(y_test[:,i], model.predict(X_test), i)
         print('Training complete.')
     
+    
     def test(self, X_test, y_test):
+        """Tests your model, populates scores."""
         for i, model in enumarate(self.models):
             print('Testing model %d for label %s.' % (i+1, self.labels[i]))
             self.scoring(y_test[:,i], model.predict(X_test), i)
         print('Testing complete')
             
-        
+            
     def get_scores(self):
+        """Returned saved scored from last test."""
         if len(self.scores) > 0:
             for score in self.scores:
                 print('Scores for label "' + str(score[0]) + '".')
@@ -72,7 +94,18 @@ class MultiLabel():
                 print('Recall: %.3f' % score[2])
                 print('F1 Score: %.3f\n' % score[3])
         else:
-            print('No score data. Scoring data is saved after model testing.')   
+            print('No score data. Scoring data is saved after model testing.')    
+            
+    def predict(self, X):
+        """Returns one numpy array with multi-label predictions."""
+        prediction = np.zeros([X.shape[0], self.num_labels])
+        for i, model in enumerate(self.models):
+            print("Predicting label %s...  " % self.labels[i], end="")
+            prediction[:,i] = model.predict(X)
+            print("Done.")
+        return prediction
+        
+		
 
 def stack_predictions(X_train, y_train, X_test, submit, K, *models):
     train_preds = pd.DataFrame(index=np.array(range(X_train.shape[0])))
